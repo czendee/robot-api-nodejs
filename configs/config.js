72dbe705d871530c5eaa1baeb40b971aab0b9fa9
Don Carlos Zendejas <czende@hotmail.com>
2020-08-26 10:45:56 -0500

    Update config.js

--- configs/config.js
+++ configs/config.js
@@ -1,3 +1,7 @@
 module.exports = {
-    llave: "miclaveultrasecreta123*"
+    llave: "miclaveultrasecreta123*",
+    mongodburl: "mongodb://tochi:tochi@ds111336.mlab.com:11336/tochi",
+    pdfpage01: "si.jpg",
+    pdfpage02: "page02.jpg",
+    pdfpage04: "page04.jpg",
 }
