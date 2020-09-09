71f9034dc3c199a01af152f1f0ea8471cee4b905
Don Carlos Zendejas <czende@hotmail.com>
2020-09-09 14:41:56 -0500

    Update indexcotiza.js

--- indexcotiza.js
+++ indexcotiza.js
@@ -338,6 +338,19 @@ app.get("/banwireapi/cotizaciones", function(req, res) {
     if (err) {
       handleError(res, err.message, "Fallo obtener cotizaciones.");
     } else {
+      // Website you wish to allow to connect
+      res.setHeader('Access-Control-Allow-Origin', '*');
+
+      // Request methods you wish to allow
+      res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+      // Request headers you wish to allow
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+      // Set to true if you need the website to include cookies in the requests sent
+      // to the API (e.g. in case you use sessions)
+      res.setHeader('Access-Control-Allow-Credentials', true); 
+      
       res.status(200).json(docs);
     }
   });
